581f17a719f363eecf400b310764fb64787ed51b
hswee92 <s2190151@siswa.um.edu.my>
2023-12-25 13:20:12 +0800

    update

--- streamlit_forex.py
+++ streamlit_forex.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime
 
 
 st.write("Hello! Welcome to Forex Prediction page!")
@@ -14,11 +15,19 @@ fig, ax = plt.subplots()
 ax.hist(rand, bins=15)
 st.pyplot(fig)
 
-
 st.write("Disclaimer: Trading involves risk. \n"
          "As a general rule, you should only trade in financial products that "
          "you are familiar with and understand the risk associated with them. \n"
          "Trade at your own risk.")
+
+while True:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    time = "Current Time = " + current_time
+    st.write(time)
+
+
+
 
 
 
