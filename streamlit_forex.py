a172f536c396df1adc7e59f3b601405690ce2bdc
hswee92 <117873041+hswee92@users.noreply.github.com>
2023-12-28 15:47:08 +0800

    Update streamlit_forex.py

--- streamlit_forex.py
+++ streamlit_forex.py
@@ -43,7 +43,7 @@ path = "./" + hist_file
 df = pd.read_csv(hist_file, delimiter=',', index_col=False)
 
 # Prepare for plot
-str_date = df['Date'].iloc[0][0:10]
+str_date = str(df['Date'].iloc[0])[0:10]
 df_datetime = pd.DataFrame()
 df_datetime['Date'] = pd.date_range(str_date, periods=1440, freq="T")
 
