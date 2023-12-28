e2b2e137f8e77d7c5f2f8b006cadff515712ff09
hswee92 <117873041+hswee92@users.noreply.github.com>
2023-12-28 12:06:58 +0800

    Update streamlit_forex.py

--- streamlit_forex.py
+++ streamlit_forex.py
@@ -1,12 +1,16 @@
 # import libraries
 import streamlit as st
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from datetime import datetime
 import time
 import warnings
 
 warnings.simplefilter('ignore')
+
+initial_state = 0
+st.title('Forex Pair Graphs')
 
 st.sidebar.title("Forex Pair")
 radio_forex = st.sidebar.radio("Pick the interested forex pair.", ["EURUSD", "GBPUSD**", "USDJPY**"], key='forex')
@@ -16,20 +20,27 @@ st.write("Hello! Welcome to Forex Prediction page!")
 
 if radio_forex == "EURUSD":
          st.write("this is EURUSD")
+         # hist_file = radio_forex[0:6] + "_historical.txt"
+
 elif radio_forex == "GBPUSD**":
          st.write("this is GBPUSD")
+         # hist_file = radio_forex[0:6] + "_historical.txt"
+         
 elif radio_forex == "USDJPY**":
          st.write("this is USDJPY")
+         # hist_file = radio_forex[0:6] + "_historical.txt"
 
-         
+hist_file = radio_forex[0:6] + "_historical.txt"
+df = pd.read_csv(hist_file, delimiter=',', index_col=False)
+st.write(df)
+
 rand = np.random.normal(1, 2, size=20)
 fig, ax = plt.subplots()
 ax.hist(rand, bins=15)
 st.pyplot(fig)
 
 
-initial_state = 0
-st.title('Displaying Time')
+
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
