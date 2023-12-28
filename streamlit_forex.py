f4cd4891ac3f868e0d23d572a8e24452918be818
hswee92 <117873041+hswee92@users.noreply.github.com>
2023-12-28 22:37:32 +0800

    update

--- streamlit_forex.py
+++ streamlit_forex.py
@@ -47,6 +47,11 @@ def prediction_table(df_prediction):
     df2_set1 = df2[['Date','Close']][0:10]
     df2_set2 = df2[['Date','Close']][10:20]
     df2_set3 = df2[['Date','Close']][20:30]
+
+    table.write(df2_set1)
+    table.write(df2_set2)
+    table.write(df2_set3)
+
     table.write(df2_set1.T)
     table.write(df2_set2.T)
     table.write(df2_set3.T)
