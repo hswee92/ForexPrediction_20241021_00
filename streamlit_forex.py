b3520ec2a1a7008179e1a7ebb5b83e4b6b925ccf
hswee92 <117873041+hswee92@users.noreply.github.com>
2023-12-28 19:40:23 +0800

    update

--- streamlit_forex.py
+++ streamlit_forex.py
@@ -23,6 +23,7 @@ def plot_graph(df,df_pred=pd.DataFrame()):
     if radio_forex == "EURUSD":
         # st.write(df.iloc[-1].T)
         # df_pred = pd.concat([df.iloc[-1].T, df_pred]).reset_index(drop=True)
+        pd.set_option('display.transpose', False)
         df_pred = pd.merge(df_pred, df.iloc[-1], how='left', on=['Date_timestamp', 'Date_timestamp'])
         st.write(df_pred)
         ax.plot(df_pred['Date_timestamp'],df_pred['Close'],label="Prediction",color='red',linewidth=2.5)
