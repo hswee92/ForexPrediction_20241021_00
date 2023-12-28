4722182721f9c96dd4d000bc40f41a481ce39968
hswee92 <117873041+hswee92@users.noreply.github.com>
2023-12-28 15:35:54 +0800

    Update streamlit_forex.py

--- streamlit_forex.py
+++ streamlit_forex.py
@@ -42,15 +42,16 @@ hist_file = radio_forex[0:6] + "_historical.txt"
 path = "./" + hist_file
 df = pd.read_csv(hist_file, delimiter=',', index_col=False)
 
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(24,10))
 
 # Plot
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(24,10))
+
 ax.plot(df['Date'],df['Close'],label="silhouette score",color=plotcolor) # marker='x' marker='.'
 ax.set(xlabel='Time')  
 ax.set(ylabel='Exchange Rate') 
 plottitle = radio_forex[0:6] + 'Latest Exchange Rate'
 ax.set_title(plottitle)
-
+st.pyplot(fig)
 
 st.table(df)
 
