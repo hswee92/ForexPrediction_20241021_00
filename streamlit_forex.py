7d14b327e116c1a2e6005f4d0cfcff362a2cb1f0
hswee92 <117873041+hswee92@users.noreply.github.com>
2024-01-15 23:27:29 +0800

    update

--- streamlit_forex.py
+++ streamlit_forex.py
@@ -179,11 +179,9 @@ st.write("**Prediction not available.")
 
 st.sidebar.title('Forex Rate Prediction')
 pred_toggle = st.sidebar.toggle("Enable Prediction",key='pred')
-st.sidebar.write(" ")
-st.sidebar.write(" ")
-st.sidebar.write(" ")
-st.sidebar.write(" ")
-st.sidebar.write(" ")
+for space in range(10):
+    st.sidebar.write(" ")
+
 st.sidebar.write("Creator: Wee Hin Sheik")
 st.sidebar.write("Email: hswee92@gmail.com")
 st.sidebar.markdown( 
