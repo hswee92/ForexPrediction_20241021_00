8b6e618f760473f81fca9524679a0d51605834a6
hswee92 <117873041+hswee92@users.noreply.github.com>
2024-01-15 23:37:55 +0800

    update

--- streamlit_forex.py
+++ streamlit_forex.py
@@ -179,17 +179,18 @@ st.write("**Prediction not available.")
 
 st.sidebar.title('Forex Rate Prediction')
 pred_toggle = st.sidebar.toggle("Enable Prediction",key='pred')
-for space in range(20):
+for space in range(25):
     st.sidebar.write(" ")
 
 st.sidebar.write("Creator: Wee Hin Sheik")
 st.sidebar.write("Email: hswee92@gmail.com")
+st.sidebar.write("Linkedin")
 st.sidebar.image("Linkedin-Logo.png")
 st.sidebar.markdown( 
     """<a href="https://www.linkedin.com/in/hin-sheik-wee-9855704b/">
     <img src="data:image/png;base64,{}" width="50">
     </a>""".format(base64.b64encode(open("Linkedin-Logo.png", "rb").read()).decode()),
-    unsafe_allow_html=True,
+    unsafe_allow_html=True
 )
 
 
