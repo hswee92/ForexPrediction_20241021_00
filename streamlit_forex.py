af2e8c5272c5ed28c16b37fad5a33e54cf3be7e9
hswee92 <117873041+hswee92@users.noreply.github.com>
2024-10-31 23:29:39 +0800

    debug

--- streamlit_forex.py
+++ streamlit_forex.py
@@ -41,7 +41,7 @@ def plot_graph(df_hist,df_predict_ori=pd.DataFrame(),check=st.session_state["pre
     if forex_pair[0:6] == "EURUSD" and pred_toggle:
 
         # "Date" "ChangePercent_1m" "Close"        
-        ax.scatter(df_predict_ori['Date'], df_predict_ori['Close'], color='red', marker='x', s=30)
+        ax.scatter(df_predict_ori['Date'], df_predict_ori['Close'], color='red', marker='x', s=5)
 
 
     ax.set(xlabel='Coordinated Universal Time, UTC')  
